86b4fdd2f5410f1ef2501d2dd5561486629af0c0
RR <randolph2894@gmail.com>
2022-01-16 17:19:22 +0200

    Fixed incompatibilities with Chart.js, as well as react app rendering of the project.

--- farmsjs/src/components/BarChart.js
+++ farmsjs/src/components/BarChart.js
@@ -1,18 +1,42 @@
-import React from 'react'
-import { Bar } from 'react-chartjs-2'
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+} from "chart.js";
 
-const BarChart = () => {
-  return (
-    <div>
-      <Bar
-        data={{
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-       }}
-       height={400}
-       width={600}
-     />
-    </div>
-  )
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: [234, 234, 45, 546, 645, 879, 765],
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: [284, 294, 745, 846, 515, 579, 165],
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+export default function App() {
+  return <Bar data={data} />;
 }
-
-export default BarChart
